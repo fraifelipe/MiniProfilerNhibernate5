da06aee188a03e4773d0a46de255c29ce80e67c4
Felipe <fraifelipe@gmail.com>
2020-04-16 00:21:39 -0300

    Init

---
+++ Infra/NHibernateFactory.cs
@@ -0,0 +1,38 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Conventions.Helpers;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+namespace MiniProfilerNhibernate5.Infra
+{
+    public class NHibernateFactory
+    {
+        private string ConnectionString { get; }
+
+        public NHibernateFactory(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+        
+        public ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure().Database(MsSqlConfiguration.MsSql2012.ConnectionString(ConnectionString)
+#if DEBUG
+                        .ShowSql()
+#endif    
+                )
+
+                .Mappings(m => m.FluentMappings
+                    .AddFromAssemblyOf<Program>()
+                    .Conventions.Setup(c =>
+                    {
+                        c.Add(DefaultLazy.Never());
+                    }))
+
+                .ExposeConfiguration(cfg => new SchemaExport(cfg)
+                    .Execute(true, false,false))
+                .BuildSessionFactory();
+        }
+    }
+}
